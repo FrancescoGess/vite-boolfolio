762f917fe3964f8903f51a7f8e646ecc6d1f75a5
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-04-05 10:56:40 +0200

    inizializzo progetto

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+  name: "HeaderApp",
+  data(){
+    
+  }
+}
+
+</script>
+
+<template>
+<h1>
+  
+</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MainApp.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+  name: "MainApp",
+  data(){
+    
+  }
+}
+
+</script>
+
+<template>
+<h1>
+  dkdsdk
+</h1>
+</template>
+
+<style scoped>
+
+</style>
