f1ea7fd74590461ce9a863759b54a0ecf98c3208
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-04-09 14:54:14 +0200

    fix nav

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,50 +1,37 @@
 <script>
-export default{
+export default {
   name: "HeaderApp",
-  data(){
-    
+  data() {
+    return {
+
+    }
   }
 }
 
 </script>
 
 <template>
-<nav
-  class="navbar navbar-expand-sm navbar-light bg-light"
->
-  <div class="container">
-    <a class="navbar-brand" href="#">Boolfolio</a>
-    <button
-      class="navbar-toggler d-lg-none"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#collapsibleNavId"
-      aria-controls="collapsibleNavId"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="collapsibleNavId">
-      <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" href="#" aria-current="page"
-            >Home
-            <span class="visually-hidden">(current)</span></a
-          >
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Projects</a>
-        </li>
-       
-      </ul>
-
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Boolfolio</a>
+      <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+        data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavId">
+        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+          <li class="nav-item">
+            <router-link :to="{ name: 'home' }">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name: 'projects'}">Projects</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
-  </div>
-</nav>
+  </nav>
 
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
