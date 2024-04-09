3bc9ea8266b73f384eb21079bed0bc39883b6304
FrancescoGess <148055980+FrancescoGess@users.noreply.github.com>
2024-04-09 14:54:26 +0200

    add router view

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <script>
+import { store } from "./store";
 import HeaderApp from './components/HeaderApp.vue';
 import MainApp from './components/MainApp.vue';
 // import Projectcard from './components/Projectcard.vue';
 
 export default {
     components: {
-        HeaderApp,
-        MainApp
+        HeaderApp
     }
 }
 </script>
 
 <template>
     <HeaderApp></HeaderApp>
+
+    <router-view></router-view>
 </template>
 
 <style>
